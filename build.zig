614450591d0b5495b527fc5ccf5c281be5fa75dd
Hendrik Nieländer <hendrik@nielaender.de>
2023-07-06 00:10:30 +0200

    feat: add basic version

--- build.zig
+++ build.zig
@@ -25,7 +25,9 @@ pub fn build(b: *std.Build) void {
     });
 
     // Add the clap package
-    exe.addAnonymousModule("clap", .{ .source_file = .{ .path = "src/deps/zig-clap/clap.zig" } });
+    exe.addAnonymousModule("clap", .{
+        .source_file = .{ .path = "libs/zig-clap/clap.zig" },
+    });
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
@@ -63,7 +65,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    unit_tests.addAnonymousModule("clap", .{ .source_file = .{ .path = "src/deps/zig-clap/clap.zig" } });
+    unit_tests.addAnonymousModule("clap", .{ .source_file = .{ .path = "libs/zig-clap/clap.zig" } });
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
 
--- src/main.zig
+++ src/main.zig
@@ -1,39 +1,56 @@
-const clap = @import("./deps/zig-clap/clap.zig");
+const clap = @import("clap");
 const std = @import("std");
+const builtin = @import("builtin");
 
 const debug = std.debug;
 const io = std.io;
 
+const VERSION = "0.0.0";
+
+const params = [_]clap.Param(clap.Help){
+    clap.parseParam("-v, --verbose Display more...") catch unreachable,
+    clap.parseParam("-h, --help Show help") catch unreachable,
+    clap.parseParam("-n, --count <usize> How many runs? Default 10") catch unreachable,
+    clap.parseParam("-f, --function <str> Function name to benchmark") catch unreachable,
+    clap.parseParam("--version Print the version and exit") catch unreachable,
+};
+
+pub const Arguments = struct {
+    verbose: bool,
+    function: ?[]const u8,
+    count: usize = 10,
+
+    pub fn parse(allocator: std.mem.Allocator) !Arguments {
+        var diag = clap.Diagnostic{};
+
+        var res = clap.parse(clap.Help, &params, clap.parsers.default, .{
+            .diagnostic = &diag,
+            .allocator = allocator,
+        }) catch |err| {
+            diag.report(io.getStdErr().writer(), err) catch {};
+            return err;
+        };
+
+        defer res.deinit();
+
+        if (res.args.version != 0) {
+            std.debug.print("Version: {s}\n", .{VERSION});
+        }
+
+        return Arguments{ .verbose = res.args.verbose != 0, .function = res.args.function, .count = if (res.args.count) |c| c else 10 };
+    }
+};
+
 pub fn main() !void {
-    // First we specify what parameters our program can take.
-    // We can use `parseParamsComptime` to parse a string into an array of `Param(Help)`
-    const params = comptime clap.parseParamsComptime(
-        \\-h, --help             Display this help and exit.
-        \\-n, --number <usize>   An option parameter, which takes a value.
-        \\-s, --string <str>...  An option parameter which can be specified multiple times.
-        \\<str>...
-        \\
-    );
+    const allocator = std.heap.page_allocator;
 
-    // Initialize our diagnostics, which can be used for reporting useful errors.
-    // This is optional. You can also pass `.{}` to `clap.parse` if you don't
-    // care about the extra information `Diagnostics` provides.
-    var diag = clap.Diagnostic{};
-    var res = clap.parse(clap.Help, &params, clap.parsers.default, .{
-        .diagnostic = &diag,
-    }) catch |err| {
-        // Report useful error and exit
-        diag.report(io.getStdErr().writer(), err) catch {};
-        return err;
-    };
-    defer res.deinit();
+    var args_result = try Arguments.parse(allocator);
 
-    if (res.args.help != 0)
-        debug.print("--help\n", .{});
-    if (res.args.number) |n|
-        debug.print("--number = {}\n", .{n});
-    for (res.args.string) |s|
-        debug.print("--string = {s}\n", .{s});
-    for (res.positionals) |pos|
-        debug.print("{s}\n", .{pos});
+    std.debug.print("verbose: {}\n", .{args_result.verbose});
+    std.debug.print("count: {}\n", .{args_result.count});
+    if (args_result.function) |function| {
+        std.debug.print("function: {s}\n", .{function});
+    } else {
+        std.debug.print("function: None\n", .{});
+    }
 }
