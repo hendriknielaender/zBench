767a6bd444830271358424e1f5ef8c920122eb2f
Hendrik Nieländer <hendrik@nielaender.de>
2023-07-14 22:41:31 +0200

    feat: add p75 p99 p995

--- examples/basic/src/main.zig
+++ examples/basic/src/main.zig
@@ -1,6 +1,18 @@
 const std = @import("std");
 const zbench = @import("zbench");
 
+pub const CustomAllocator = struct {
+    allocator: std.mem.Allocator,
+
+    pub fn create() CustomAllocator {
+        return CustomAllocator{ .allocator = std.heap.page_allocator };
+    }
+
+    pub fn as_mut(self: *CustomAllocator) *std.mem.Allocator {
+        return &self.allocator;
+    }
+};
+
 fn myBenchmark(b: *zbench.Benchmark) void {
     b.timer.start(); // Start the timer
     // Perform the benchmark here
@@ -9,16 +21,17 @@ fn myBenchmark(b: *zbench.Benchmark) void {
         // Simulating an operation
         b.incrementOperations(1);
     }
+    b.timer.stop(); // Stop the timer
 }
 
 pub fn main() !void {
+    var customAllocator = CustomAllocator.create();
+    var resultsAlloc = std.ArrayList(zbench.BenchmarkResult).init(customAllocator.allocator);
+    var bench = try zbench.Benchmark.init("My Benchmark", customAllocator.as_mut());
+
     var benchmarkResults = zbench.BenchmarkResults{
-        .results = std.ArrayList(zbench.BenchmarkResult).init(std.heap.page_allocator),
+        .results = resultsAlloc,
     };
 
-    var bench = zbench.Benchmark.init("myBenchmark");
     try zbench.run(myBenchmark, &bench, &benchmarkResults);
-
-    // After all benchmarks have been run...
-    try benchmarkResults.prettyPrint();
 }
--- zbench.zig
+++ zbench.zig
@@ -18,23 +18,54 @@ pub const Timer = struct {
     pub fn reset(self: *Timer) void {
         self.startTime = std.time.milliTimestamp();
     }
+
+    pub fn stop(self: *Timer) void {
+        self.startTime = 0;
+    }
 };
 
-// B is passed to each benchmark function. It controls the benchmark
-// and reports the results.
 pub const Benchmark = struct {
     name: []const u8,
     timer: Timer,
     totalOperations: usize = 0,
+    minDuration: u64 = 18446744073709551615,
+    maxDuration: u64 = 0,
+    durations: std.ArrayList(u64),
 
     // Initialization function
-    pub fn init(name: []const u8) Benchmark {
+    pub fn init(name: []const u8, allocator: *std.mem.Allocator) !Benchmark {
         var bench = Benchmark{
             .name = name,
             .timer = Timer{ .startTime = std.time.milliTimestamp() },
+            .durations = std.ArrayList(u64).init(allocator.*),
         };
         bench.timer.start();
         return bench;
+    }
+
+    // Start the benchmark
+    pub fn start(self: *Benchmark) void {
+        self.timer.start();
+    }
+
+    // Stop the benchmark and record the duration
+    pub fn stop(self: *Benchmark) !void {
+        try self.durations.append(self.timer.elapsed());
+        const elapsedDuration = self.timer.elapsed();
+        try self.durations.append(elapsedDuration);
+
+        if (elapsedDuration < self.minDuration) self.minDuration = elapsedDuration;
+        if (elapsedDuration > self.maxDuration) self.maxDuration = elapsedDuration;
+
+        self.totalOperations += 1;
+    }
+
+    // Reset the benchmark
+    pub fn reset(self: *Benchmark) void {
+        self.timer.reset();
+        self.totalOperations = 0;
+        self.minDuration = 18446744073709551615;
+        self.maxDuration = 0;
     }
 
     // Function to get elapsed time since benchmark start
@@ -49,9 +80,43 @@ pub const Benchmark = struct {
     pub fn report(self: *Benchmark) void {
         std.debug.print("Total operations: {}\n", .{self.totalOperations});
     }
+
+    pub const Percentiles = struct {
+        p75: u64,
+        p99: u64,
+        p995: u64,
+    };
+
+    // Calculate the p75, p99, and p995 durations
+    pub fn calculatePercentiles(self: Benchmark) Percentiles {
+        const p75Index = (self.totalOperations * 75) / 100;
+        const p99Index = (self.totalOperations * 99) / 100;
+        const p995Index = (self.totalOperations * 995) / 1000;
+
+        const p75 = self.durations[p75Index];
+        const p99 = self.durations[p99Index];
+        const p995 = self.durations[p995Index];
+
+        return Percentiles{ .p75 = p75, .p99 = p99, .p995 = p995 };
+    }
+
+    pub fn prettyPrint(self: Benchmark) void {
+        std.debug.print("--------------------------------------------------------------------------------------\n", .{});
+        std.debug.print("Benchmark: {s}\n", .{self.name});
+    }
+
+    // Calculate the average duration
+    pub fn calculateAverage(self: Benchmark) f64 {
+        var sum: u64 = 0;
+        for (self.durations) |duration| {
+            sum += duration;
+        }
+        const total_operations_f64: f64 = @floatFromInt(self.totalOperations);
+        const sum_f64: f64 = @floatFromInt(sum);
+        return sum_f64 / total_operations_f64;
+    }
 };
 
-// BenchFunc is a function that performs a benchmark.
 pub const BenchFunc = fn (*Benchmark) void;
 
 pub const BenchmarkResult = struct {
@@ -62,7 +127,6 @@ pub const BenchmarkResult = struct {
 pub const BenchmarkResults = struct {
     results: std.ArrayList(BenchmarkResult),
 
-    // Calculate the relative performance and assign a color accordingly.
     pub fn getColor(self: *const BenchmarkResults, duration: u64) c.Color {
         const max_duration = @max(self.results.items[0].duration, self.results.items[self.results.items.len - 1].duration);
         const min_duration = @min(self.results.items[0].duration, self.results.items[self.results.items.len - 1].duration);
@@ -70,33 +134,33 @@ pub const BenchmarkResults = struct {
         if (duration <= min_duration) return c.Color.green;
         if (duration >= max_duration) return c.Color.red;
 
-        const prop = (duration - min_duration) * 100 / (max_duration - min_duration + 1); // Multiply duration by 100 to create a percentage
+        const prop = (duration - min_duration) * 100 / (max_duration - min_duration + 1);
 
-        if (prop < 50) return c.Color.green; // Compare to 50, equivalent to comparing prop/100 < 0.5
-        if (prop < 75) return c.Color.yellow; // Compare to 75, equivalent to comparing prop/100 < 0.75
+        if (prop < 50) return c.Color.green;
+        if (prop < 75) return c.Color.yellow;
 
         return c.Color.red;
     }
 
-    // Pretty-print the benchmark results.
     pub fn prettyPrint(self: BenchmarkResults) !void {
         const stdout = std.io.getStdOut().writer();
+        std.debug.print("--------------------------------------------------------------------------------------\n", .{});
+
         for (self.results.items) |result| {
-            const color = self.getColor(result.duration);
-            const formatted = try std.fmt.format(stdout, "{s}Benchmark: {s}\nDuration: {d} ms\n", .{ color.code(), result.name, result.duration });
-            try stdout.print("{}", .{formatted});
+            try stdout.print("{s}", .{result.name});
         }
     }
 };
 
-// The benchmark function that takes a benchmark function and a name,
-// performs the benchmark, and reports the results.
 pub fn run(comptime func: BenchFunc, bench: *Benchmark, benchResult: *BenchmarkResults) !void {
     const iterations: usize = 1000;
     var j: usize = 0;
     while (j < iterations) : (j += 1) {
+        bench.start();
         func(bench);
+        try bench.stop(); // Propagate error
     }
+    bench.prettyPrint();
     const elapsed = bench.elapsed();
     std.debug.print("{s}: Elapsed time: {d} ms\n", .{ bench.name, elapsed });
     bench.report();
