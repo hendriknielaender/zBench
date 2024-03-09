1237f4beeb8e78f0033c3ecc2536e51de97999b5
hndrk <51416554+hendriknielaender@users.noreply.github.com>
2024-03-10 00:33:41 +0100

    feat(benchmark): add lifecycle hooks for enhanced benchmark control (#56)
    * feat(benchmark): add lifecycle hooks for enhanced benchmark control
    
    * chore: add hooks to README.md

--- examples/basic.zig
+++ examples/basic.zig
@@ -13,6 +13,14 @@ fn helloWorld() []const u8 {
     return "Hello, world!";
 }
 
+fn beforeAllHook() void {
+    std.debug.print("Starting benchmarking...\n", .{});
+}
+
+fn afterAllHook() void {
+    std.debug.print("Finished benchmarking.\n", .{});
+}
+
 fn myBenchmark(_: *zbench.Benchmark) void {
     _ = helloWorld();
 }
@@ -21,7 +29,14 @@ test "bench test basic" {
     const resultsAlloc = std.ArrayList(zbench.BenchmarkResult).init(test_allocator);
     var benchmarkResults = zbench.BenchmarkResults.init(resultsAlloc);
     defer benchmarkResults.deinit();
-    var bench = try zbench.Benchmark.init("My Benchmark", test_allocator, .{ .iterations = 10, .display_system_info = true });
+    var bench = try zbench.Benchmark.init("My Benchmark", test_allocator, .{
+        .iterations = 10,
+        .display_system_info = true,
+        .hooks = .{
+            .beforeAll = beforeAllHook,
+            .afterAll = afterAllHook,
+        },
+    });
 
     try zbench.run(myBenchmark, &bench, &benchmarkResults);
     try benchmarkResults.prettyPrint();
