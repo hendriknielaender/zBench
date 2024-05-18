7dc1d6bb03d95659256b366d0dc0f3dfb72fcd0a
FObersteiner <f.obersteiner@posteo.de>
2024-05-18 18:40:11 +0200

    revise baseline example

--- examples/baseline.zig
+++ examples/baseline.zig
@@ -10,31 +10,41 @@ test "benchmark timer baseline" {
     var bench = zbench.Benchmark.init(std.testing.allocator, .{});
     defer bench.deinit();
 
-    try bench.add("Bench baseline, n=10 ", noop, .{
+    try bench.add("Correction off, n=10 ", noop, .{
         .iterations = 10,
         .baseline_correction = false,
     });
-    try bench.add("Bench baseline, n=10 ", noop, .{
+    try bench.add("Correction on,  n=10 ", noop, .{
         .iterations = 10,
         .baseline_correction = true,
     });
 
-    try bench.add("Bench baseline, n=1k ", noop, .{
+    try bench.add("Correction off, n=1k ", noop, .{
         .iterations = 1_000,
         .baseline_correction = false,
     });
-    try bench.add("Bench baseline, n=1k ", noop, .{
+    try bench.add("Correction on,  n=1k ", noop, .{
         .iterations = 1_000,
         .baseline_correction = true,
     });
 
-    try bench.add("Bench baseline, n=10k", noop, .{
+    try bench.add("Correction off, n=10k", noop, .{
         .iterations = 10_000,
         .baseline_correction = false,
     });
 
-    try bench.add("Bench baseline, n=10k", noop, .{
+    try bench.add("Correction on,  n=10k", noop, .{
         .iterations = 10_000,
+        .baseline_correction = true,
+    });
+
+    try bench.add("Correction off, n=1M ", noop, .{
+        .iterations = 1_000_000,
+        .baseline_correction = false,
+    });
+
+    try bench.add("Correction on,  n=1M ", noop, .{
+        .iterations = 1_000_000,
         .baseline_correction = true,
     });
 
