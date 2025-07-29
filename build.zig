7069bb9c3e2e4773ff93995a85238b8e08039ba0
hndrk <51416554+hendriknielaender@users.noreply.github.com>
2025-07-29 18:38:56 +0200

    chore: bump version to 0.11.0

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const log = std.log.scoped(.zbench_build);
 
-const version = std.SemanticVersion{ .major = 0, .minor = 10, .patch = 0 };
+const version = std.SemanticVersion{ .major = 0, .minor = 11, .patch = 0 };
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
