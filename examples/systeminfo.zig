4981b5a58277ecf65959309dc67c3d546a8be3cb
FObersteiner <florian.obersteiner@kit.edu>
2026-03-25 20:13:51 +0100

    use consistent variables in main

--- examples/systeminfo.zig
+++ examples/systeminfo.zig
@@ -3,8 +3,9 @@ const zbench = @import("zbench");
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
-    var stdout: std.Io.File.Writer = std.Io.File.stdout().writerStreaming(io, &.{});
-    const writer = &stdout.interface;
+    const stdout: std.Io.File = .stdout();
+    var filewriter: std.Io.File.Writer = stdout.writerStreaming(io, &.{});
+    const writer: *std.Io.Writer = &filewriter.interface;
 
     try writer.print("\n\n{f}\n", .{try zbench.getSystemInfo()});
 }
